9de08375f62af44c26ea3540276177d823f6b8b7
mohamednaji7 <n4jidx@gmail.com>
2025-03-25 23:47:10 +0200

    Refactor

--- src/example-rag.py
+++ src/example-rag.py
@@ -1,15 +1,21 @@
 
 
 from rag import (
-    process_chunk_and_embed,
+    make_metdata,
+    add_embedding,
     supabase_upload_records
 )
 
-# # Process the files
-# process_chunk_and_embed(
-#     'rag_data/input/results_deduplicated.json',
-#     'rag_data/output/records.json'      
-#     )
+make_metdata(
+    'rag_data/input/results_deduplicated.json',
+    'rag_data/output/records_id_metadata.json'      
+)
+
+
+add_embedding(
+    'rag_data/output/records_id_metadata.json' ,
+    'rag_data/output/records.json'      
+    )
 
 supabase_upload_records(
     'rag_data/output/records.json',
