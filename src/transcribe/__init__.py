ea3a9fe1cd6e4824077bafad77e2472d7775ba5d
mohamednaji7 <n4jidx@gmail.com>
2025-04-07 14:07:17 +0200

    Refactor imports in transcribe module: consolidate imports from preprocess for improved readability and maintainability

--- src/transcribe/__init__.py
+++ src/transcribe/__init__.py
@@ -1,1 +1,11 @@
-from .preprocess import test_writing_empty_files, dir_tree_to_json
+from .preprocess import (
+    test_writing_empty_files,
+    dir_tree_to_json,
+
+    get_file_duration,
+    has_audio_stream,
+    get_durations,
+    get_names,
+    check_files_states,
+    save_plot_file_durations
+)
